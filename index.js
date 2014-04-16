d94ec126f92bdb5394a80d05aa9e474f5a242e60
Chengkun <chenchengkun@gmail.com>
2014-04-16 15:14:12 +0800

    change public folder name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3001);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
@@ -14,8 +14,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/vendor'));
-  app.use(express.static(path.join(__dirname, 'vendor')));
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
