a01653d6a1a3a764806d77470c7dddb9d4888800
Chengkun <chenchengkun@gmail.com>
2014-04-18 00:24:57 +0800

    add twenty theme.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/twenty', routes.twenty);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 exports.index = function(req, res){
   res.render('index');
 };
+exports.twenty = function(req, res){
+  res.render('twenty');
+}
